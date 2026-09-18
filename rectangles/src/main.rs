957f0bcd3dc1ed6c75d674c9c74abc76e5cde5e6
Chase Gladish <chase.gladish@gmail.com>
2026-09-17 23:09:51 -0700

    Rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,64 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    let rect1 = Rectangle {
+        width: dbg!(2 * width1),
+        height: height1
+    };
+
+    println!(
+        "The area of the rectangle is {}",
+        area(&rect1)
+    );
+
+    println!("Rect1 is {rect1:#?}");
+
+    println!(
+        "The area of the rectangle is {}",
+        rect1.area()
+    );
+
+    println!(
+        "Calling width func {}",
+        rect1.width()
+    );
+    println!(
+        "Calling width func via ref {}",
+        (&rect1).width()
+    );
+
+    let square = Rectangle::square(123);
+    println!(
+        "Square {square:?}"
+    );
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
+impl Rectangle{
+    fn width(&self) -> u32 {
+        self.width * 10
+    }
 }
